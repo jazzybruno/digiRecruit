e8e56fd833f971e32bae4b9da28c72a36da4663c
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-07-01 16:26:31 +0200

    Added the home page file and all other configuration

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return ( 
+        <div className="w-[100%] h-[100%]  bg-pink-300">
+            This is the home page
+        </div>
+     );
+}
+ 
+export default HomePage;
