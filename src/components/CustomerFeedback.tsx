0e4b31180b7f08510252f37bd02722982d97ecf5
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-07-02 12:55:28 +0200

    Added the footer at the home page

--- src/components/CustomerFeedback.tsx
+++ src/components/CustomerFeedback.tsx
@@ -1,27 +1,30 @@
-import Avatar from '@mui/material/Avatar';
-import { BsStarFill } from 'react-icons/bs';
-import me from "../images/me.jpg"
+import Avatar from "@mui/material/Avatar";
+import { BsStarFill } from "react-icons/bs";
+import me from "../images/me.jpg";
 import { styled } from "@mui/material/styles";
 
 const LargeAvatar = styled(Avatar)(({ theme }) => ({
-    width: theme.spacing(8),
-    height: theme.spacing(8),
-  }));
+  width: theme.spacing(8),
+  height: theme.spacing(8),
+}));
 
 const CustomerFeedback = () => {
-    return ( 
-        <div className="bg-white rounded-xl shadow-md w-[30%]">
-            <div className='flex  gap-5 flex-row items-center pl-10 pt-10 pb-2'>
-             <LargeAvatar alt="Bemy Sharp" src={me} />
-             <BsStarFill className="text-yellow-400 text-xl" />
-             <p className='font-bold text-xl text-black'>4.8</p>
-            </div>
-            <div className='flex flex-col space-y-3 pb-10 pl-10 pr-2'>
-                <h1 className='text-xl font-bold '>Customer Name</h1>
-                <h1 className='text-xl'>Customer Feedback Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
-            </div>
-        </div>
-     );
-}
- 
+  return (
+    <div className="bg-white rounded-xl shadow-md w-[30%]">
+      <div className="flex  gap-5 flex-row items-center pl-10 pt-10 pb-2">
+        <LargeAvatar alt="Bemy Sharp" src={me} />
+        <BsStarFill className="text-yellow-400 text-xl" />
+        <p className="font-bold text-xl text-black">4.8</p>
+      </div>
+      <div className="flex flex-col space-y-3 pb-10 pl-10 pr-2">
+        <h1 className="text-xl font-bold ">Customer Name</h1>
+        <h1 className="text-xl">
+          Customer Feedback Lorem ipsum dolor sit amet consectetur adipisicing
+          elit.{" "}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
 export default CustomerFeedback;
--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,26 +1,80 @@
+import {
+  BiLogoFacebookCircle,
+  BiLogoInstagram,
+  BiLogoLinkedin,
+  BiLogoTwitter,
+} from "react-icons/bi";
+
 const FooterComponent = () => {
-    return ( 
-        <div className="w-[100%] bg-[#0b182c] text-white flex flex-row justify-center items-center">
-            <div className="w-[40%] flex flex-col ">
-              <div>  <p>Subscribe to our newsletter</p>
-                <div>
-                    <input type="text" name="" id="" />
-                    <button>Submit</button>
-                </div>
-                </div>
-                <div>
-                    <p>Follow us on social media</p>
-                    <div className="flex flex-row justify-start items-center~">
-                        <div className="w-[50px] h-[50px] bg-white rounded-full"></div>
-                        <div className="w-[50px] h-[50px] bg-white rounded-full"></div>
-                        <div className="w-[50px] h-[50px] bg-white rounded-full"></div>
-                        <div className="w-[50px] h-[50px] bg-white rounded-full"></div>
-                        </div>
-                </div>
-            </div>
-            <div className="w-[60%] flex flex-col">Hello World this is the second part</div>
+  const iconsStyle = "text-4xl text-white";
+  return (
+    <div className="w-[100%] bg-[#0b182c] text-white flex flex-row justify-center items-center">
+      <div className="w-[40%] flex flex-col justify-start p-10 space-y-12 ">
+        <div className="flex flex-col space-y-2">
+          {" "}
+          <p className=" text-white font-bold text-xl">
+            Subscribe to our newsletter
+          </p>
+          <div>
+            <input
+              type="text"
+              name=""
+              id=""
+              value={"Enter the Email?"}
+              className="p-2 w-[50%] bg-[#242e42] rounded-sm text-white"
+            />
+            <button className=" bg-customTeal py-2 px-5 rounded-sm border-2 ml-2 font-bold">
+              Submit
+            </button>
+          </div>
         </div>
-     );
-}
- 
+        <div>
+          <p className="text-xl font-bold text-white">
+            Follow us on social media
+          </p>
+          <div className="flex flex-row justify-start items-center space-x-5">
+            <BiLogoFacebookCircle className={iconsStyle} />
+            <BiLogoInstagram className={iconsStyle} />
+            <BiLogoLinkedin className={iconsStyle} />
+            <BiLogoTwitter className={iconsStyle} />
+          </div>
+        </div>
+      </div>
+      <div className="w-[60%] flex flex-row p-20 space-x-20">
+        <div className="flex flex-col space-y-3">
+          <h1 className="text-xl font-bold ">Services</h1>
+          <p className=" font-extralight">Email Marketing</p>
+          <p className=" font-extralight">Campaigns</p>
+          <p className=" font-extralight">Campaigns</p>
+          <p className=" font-extralight">Bradings</p>
+          <p className=" font-extralight">Offline</p>
+        </div>
+
+        <div className="flex flex-col space-y-3">
+          <h1 className="text-xl font-bold ">About</h1>
+          <p className=" font-extralight">Our Story</p>
+          <p className=" font-extralight">Benefits</p>
+          <p className=" font-extralight">Teams</p>
+          <p className=" font-extralight">Careers</p>
+        </div>
+
+        <div className="flex flex-col space-y-3">
+          <h1 className="text-xl font-bold ">Navigation</h1>
+          <p className=" font-extralight">Email Marketing</p>
+          <p className=" font-extralight">Campaigns</p>
+          <p className=" font-extralight">Campaigns</p>
+          <p className=" font-extralight">Bradings</p>
+          <p className=" font-extralight">Offline</p>
+        </div>
+
+        <div className="flex flex-col space-y-3">
+          <h1 className="text-xl font-bold ">Help</h1>
+          <p className=" font-extralight">FAQs</p>
+          <p className=" font-extralight">Contact Us</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default FooterComponent;
